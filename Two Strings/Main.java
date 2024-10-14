8df078ae29f7a2d956e2c8a4d42e572cb34deb98
KCSAINITHIN <sainithin.cs23@bmsce.ac.in>
2024-10-14 15:32:18 +0530

    Add files via upload

---
+++ Two Strings/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+class Book {
+    private String name;
+    private String author;
+    private int price;
+    private int numPages;
+
+    public Book(String name, String author, int price, int numPages) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.numPages = numPages;
+    }
+
+    public String toString() {
+        return "Book name: " + this.name + "\n" +
+               "Author name: " + this.author + "\n" +
+               "Price: " + this.price + "\n" +
+               "Number of pages: " + this.numPages + "\n";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.print("Enter the number of books: ");
+        int n = Integer.parseInt(s.nextLine()); // Use nextLine and parse to int
+
+        Book[] books = new Book[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter details for book " + (i + 1) + ":");
+            System.out.print("Name: ");
+            String name = s.nextLine(); // Read full name
+            System.out.print("Author: ");
+            String author = s.nextLine(); // Read full author name
+            System.out.print("Price: ");
+            int price = Integer.parseInt(s.nextLine()); // Parse price from string
+            System.out.print("Number of pages: ");
+            int numPages = Integer.parseInt(s.nextLine()); // Parse number of pages from string
+
+            books[i] = new Book(name, author, price, numPages);
+        }
+
+        System.out.println("\nBook Details:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(books[i].toString());
+        }
+        
+        s.close(); // Close the scanner
+    }
+}
+
