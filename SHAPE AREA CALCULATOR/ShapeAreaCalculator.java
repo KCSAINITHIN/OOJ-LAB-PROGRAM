af87cbb0c342007035ec5f354d518b8bca2228a1
KCSAINITHIN <sainithin.cs23@bmsce.ac.in>
2024-10-21 14:53:51 +0530

    Add files via upload
    SHAPE AREA CALCULATOR

---
+++ SHAPE AREA CALCULATOR/ShapeAreaCalculator.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+class InputScanner {
+    protected Scanner scanner;
+
+    public InputScanner() {
+        scanner = new Scanner(System.in);
+    }
+
+    public int getInput(String message) {
+        System.out.print(message);
+        return scanner.nextInt();
+    }
+}
+
+
+abstract class Shape extends InputScanner {
+    protected int dimension1; 
+    protected int dimension2; 
+
+    public abstract void printArea();
+}
+
+
+class Rectangle extends Shape {
+    public Rectangle() {
+        this.dimension1 = getInput("Enter length of the rectangle: ");
+        this.dimension2 = getInput("Enter width of the rectangle: ");
+    }
+
+    @Override
+    public void printArea() {
+        int area = dimension1 * dimension2;
+        System.out.println("Area of Rectangle: " + area);
+    }
+}
+
+
+class Triangle extends Shape {
+    public Triangle() {
+        this.dimension1 = getInput("Enter base of the triangle: ");
+        this.dimension2 = getInput("Enter height of the triangle: ");
+    }
+
+    @Override
+    public void printArea() {
+        double area = 0.5 * dimension1 * dimension2;
+        System.out.println("Area of Triangle: " + area);
+    }
+}
+
+
+class Circle extends Shape {
+    public Circle() {
+        this.dimension1 = getInput("Enter radius of the circle: ");
+    }
+
+    @Override
+    public void printArea() {
+        double area = Math.PI * dimension1 * dimension1;
+        System.out.println("Area of Circle: " + area);
+    }
+}
+
+
+public class ShapeAreaCalculator {
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle();
+        rectangle.printArea();
+
+        Shape triangle = new Triangle();
+        triangle.printArea();
+
+        Shape circle = new Circle();
+        circle.printArea();
+    }
+}
