6ca826506c746c342c510a97dca28ca607f5b750
KCSAINITHIN <sainithin.cs23@bmsce.ac.in>
2024-11-29 21:56:15 +0530

    DivisionApp.java

--- DivissionApp/DivisionApp.java
+++ DivissionApp/DivisionApp.java
@@ -69,12 +69,12 @@ public class DivisionApp {
             }
         });
 
-        // Make the frame visible
+
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        // Initialize the app and create the UI
+
         new DivisionApp();
     }
 }
